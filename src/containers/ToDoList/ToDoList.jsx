5f90becf00d30eceaf391b8cb1707a63d95c5cc7
Boris K. Nekezov <boris.nekezov@gmail.com>
2020-10-20 09:04:59 +0300

    Fix response error, uncomment get and post and add some test logs

--- src/containers/ToDoList/ToDoList.jsx
+++ src/containers/ToDoList/ToDoList.jsx
@@ -11,31 +11,35 @@ class ToDoList extends Component {
   }
 
   componentDidMount() {
-    console.log(`[componentDidMount]`);
-    // console.log(`instance`);
-    // console.log(instance);
-    // instance.get("/data.json").then((response) => {
-    //   // put the data into array
-    //   const fetchedData = [];
-    //   for (let key in response.data) {
-    //     fetchedData.push({...response.data[key], id: key})
-    //   }
-    //   // set the state of todos to be fetchedData
-    //   this.setState({
-    //     results: fetchedData
-    //   })
-    // })
+    // console.log(`[componentDidMount]`);
+    instance.get("/todos.json").then((response) => {
+      // console.log(`response`);
+      // console.log(response);
+      // put the data into array
+      const fetchedData = [];
+      for (let key in response.data) {
+        // console.log('key', key);
+        // console.log('...response.data[key]', {...response.data[key]});
+        // console.log(`===SEPARATOR==============================`);
+        fetchedData.push({...response.data[key], id: key})
+      }
+      // console.log('fetchedData', fetchedData);
+      // set the state of todos to be fetchedData
+      this.setState({
+        results: fetchedData
+      })
+    })
   }
 
   handleChange = event => {
-    console.log(`[handleChange]`);
+    // console.log(`[handleChange]`);
     this.setState({
       [event.currentTarget.name]: event.currentTarget.value
     });
   }
 
   handlePost = event => {
-    console.log(`[handlePost]`);
+    // console.log(`[handlePost]`);
     event.preventDefault();
     // add the data we want to post
     const Data = {
@@ -44,18 +48,19 @@ class ToDoList extends Component {
     };
 
     // push this data to the back-end (post($endpoint) is available in axios)
-    instance.post("/data.json", Data).then((response) => {
-      console.log(response);
-      // const todos = [
-      //   ...this.state.todos,
-      //   { ...Data, id: response.data.title }
-      // ];
-      // this.setState({
-      //   title: "",
-      //   todos: todos
-      // })
+    instance.post("/todos.json", Data).then((response) => {
+      // console.log(response);
+      // response.data.name => -MK3VSi5XrApFPQ6oh1C => the key given from firebase
+      const todos = [
+        ...this.state.todos,
+        { ...Data, id: response.data.name }
+      ];
+      this.setState({
+        title: "",
+        todos: todos
+      })
+      // console.log(this.state.todos);
       // console.log('successfull post request!')
-
     });
   }
 
