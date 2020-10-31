ffb7d8586abb242dce42390a215fb2de91ff8965
Boris K. Nekezov <boris.nekezov@gmail.com>
2020-10-31 12:49:07 +0200

    Fix error in header task add componenet

--- src/components/Header/HeaderTaskAdd/HeaderTaskAdd.jsx
+++ src/components/Header/HeaderTaskAdd/HeaderTaskAdd.jsx
@@ -6,24 +6,25 @@ import Input from '../../UI/Input/Input';
 export default class HeaderTaskAdd extends Component {
 	render() {
 		const { titleAdd, handleChange, handlePost } = this.props;
-		return;
-		<form autoComplete="off" onSubmit={handlePost}>
-			<div className="input-group mb-2">
-				<div className="input-group-prepend">
-					<Button kind="success" type="submit">
-						<Icon iconName="plus" modifiers="mr-sm-2" />
-						<span className="d-none d-sm-inline">Add new task</span>
-					</Button>
+		return (
+			<form autoComplete="off" onSubmit={handlePost}>
+				<div className="input-group mb-2">
+					<div className="input-group-prepend">
+						<Button kind="success" type="submit">
+							<Icon iconName="plus" modifiers="mr-sm-2" />
+							<span className="d-none d-sm-inline">Add new task</span>
+						</Button>
+					</div>
+					<Input
+						type="text"
+						placeholder="Please enter task title!"
+						value={titleAdd}
+						onChange={handleChange}
+						name="titleAdd"
+						autoFocus
+					/>
 				</div>
-				<Input
-					type="text"
-					placeholder="Please enter task title!"
-					value={titleAdd}
-					onChange={handleChange}
-					name="titleAdd"
-					autoFocus
-				/>
-			</div>
-		</form>;
+			</form>
+		);
 	}
 }
